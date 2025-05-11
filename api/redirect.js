ab62f993cc596290fd1198a36260fec77c4ea324
Thirteen𝕏 <65433734+ThirteenXXX@users.noreply.github.com>
2025-05-12 00:44:23 +0700

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,10 +1,7 @@
 export default function handler(req, res) {
-  
-  const encoded = 'aHR0cHM6Ly96ZXByYS1tYWludGVuYW5jZS52ZXJjZWwuYXBw';
-  const decodedUrl = Buffer.from(encoded, 'base64').toString('utf-8');
-
+ 
   res.writeHead(302, {
-    Location: decodedUrl
+    Location: 'index.html' 
   });
   res.end();
 }
